acbf1cac77882e81c56ee351bec33fd21f55b16a
Berkantagur <berkantagur6@gmail.com>
2023-01-02 22:10:00 +0300

    c

---
+++ cube.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int cube(int number){
+
+    int result=number*number*number;
+
+    return result;
+}
+
+
+int main(){
+
+int n;
+
+printf("Please enter a number:");
+scanf("%d",&n);
+
+printf("RESULT:%d",cube(n));
+
+    return 0;
+}
