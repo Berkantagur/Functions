3f4039e591a992b8698c37bfe6dd57fcc3b2bada
Berkantagur <berkantagur6@gmail.com>
2023-01-04 22:41:41 +0300

    c

---
+++ palindrome.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void isItPalindrome(int x){
+
+int remainder, reversed=0, n;
+n=x;
+while(x!=0){
+    
+    remainder=x%10;
+    reversed = reversed*10 + remainder;
+    x/=10;
+}
+if (reversed == n){
+    printf("Palindrome");
+}
+else 
+    printf("Not Palindrome");
+
+}
+
+int main(){
+
+int c;
+srand(time(NULL));
+
+c = 100 + rand()%(999-100+1);
+printf("%d is ",c);
+isItPalindrome(c);
+
+return 0;
+}
+
